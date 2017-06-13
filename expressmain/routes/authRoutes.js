1d2960955728fd9af92fdea8ed1e497be15073fc
Student <student@GAs-MacBook-Air-3.local>
2017-06-13 11:07:55 -0400

    pushing to heroku

---
+++ expressmain/routes/authRoutes.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+
+const passport = require('../services/auth/local');
+const authHelpers = require('../services/auth/auth-helpers');
+
+// LOGIN ROUTE
+router.post('/login',
+  // passport.authenticate('local', { failWithError: true }),
+  function(req, res) {
+    // If this function gets called, authentication was successful.
+    // `req.user` contains the authenticated user.
+    res.status(201).send({ user_profile: req.user, loggedIn: true });
+  },
+  function(err, req, res, next) {
+    // handle error
+    if (err) { return res.status(200).send({ status: "Invalid Credentials", loggedIn: false }); }
+  });
+
+// // LOGOUT ROUTE
+// router.get('/logout', (req, res) => {
+//   req.logout();
+//   res.status(200).json({
+//     loggedIn: false
+//   });
+// });
+
+module.exports = router;
